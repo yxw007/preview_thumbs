b7841de5b0044368e2a647c3f17953348356a32a
75838259@qq.com <75838259@qq.com>
2021-09-18 18:01:10 +0800

    商品详情，预览图切换组件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <demo />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Demo from "./components/Demo.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Demo,
   },
 };
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Demo.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="contain">
+    <div class="tab">
+      <span v-for="n in 6" :key="n" @click="changePreview(n - 1)"
+        >{{ n - 1 }}张预览图</span
+      >
+      <span></span>
+    </div>
+    <div class="wrapper">
+      <div class="list">
+        <span class="pre" @click="goPreview">pre</span>
+        <div class="bs-wrapper">
+          <ul class="content">
+            <li
+              class="item"
+              v-for="(item, index) in previews"
+              :key="index"
+              :style="{
+                border:
+                  choice_index === index
+                    ? '1px solid #007dff'
+                    : '1px solid #e5e5e5',
+              }"
+              @click="choicePreview(index)"
+            >
+              <img :src="item" draggable="false" />
+            </li>
+          </ul>
+        </div>
+        <span class="next" @click="goNextview">next</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "@better-scroll/core";
+
+export default {
+  name: "Demo",
+  data() {
+    return {
+      source_previews: [
+        "https://q.kuxaunxuan.cn/image/20210913/6fd0fde98338e64d74c4ae93c1c9bc2a.png?x-oss-process=image/resize,m_mfit,h_80,w_80,limit_0",
+        "https://q.kuxaunxuan.cn/image/20210916/aaaed8561e1a275e165694c4f6c40183.png?x-oss-process=image/resize,m_mfit,h_80,w_80,limit_0",
+        "https://q.kuxaunxuan.cn/image/20210916/172455b9f6c7195c6e407ec6a2d251d9.png?x-oss-process=image/resize,m_mfit,h_80,w_80,limit_0",
+        "https://q.kuxaunxuan.cn/image/20210916/20b53059f1c35052c204f1560a523e54.png?x-oss-process=image/resize,m_mfit,h_80,w_80,limit_0",
+        "https://q.kuxaunxuan.cn/image/20210916/789b94192df5f7a1a966fc322cc870be.png?x-oss-process=image/resize,m_mfit,h_80,w_80,limit_0",
+      ],
+      previews: [],
+      choice_index: 0,
+      bsScroll: undefined,
+    };
+  },
+  mounted() {
+    this.bsScroll = new BScroll(".bs-wrapper", {
+      click: true,
+      scrollX: true,
+      disableMouse: false,
+      disableTouch: false,
+    });
+  },
+  methods: {
+    changePreview(index) {
+      let len = this.source_previews.length;
+      this.previews = this.source_previews.slice(len - index);
+      this.choice_index = 0;
+      if (this.bsScroll) {
+        setTimeout(() => {
+          this.bsScroll.refresh();
+        }, 40);
+      }
+    },
+    choicePreview(index) {
+      this.choice_index = index;
+    },
+    goPreview() {
+      this.choice_index =
+        this.choice_index - 1 < 0
+          ? this.previews.length - 1
+          : this.choice_index - 1;
+    },
+    goNextview() {
+      this.choice_index =
+        this.choice_index + 1 >= this.previews.length
+          ? 0
+          : this.choice_index + 1;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.contain {
+  .tab {
+    margin-bottom: 40px;
+    span {
+      margin-right: 20px;
+    }
+    span:nth-child(2n) {
+      border: 1px solid #00ff00;
+    }
+    span:nth-child(2n + 1) {
+      border: 1px solid #ff0000;
+    }
+  }
+
+  .wrapper {
+    .list {
+      width: 476px;
+      display: flex;
+      margin: 0 auto;
+      justify-content: space-between;
+      span {
+        width: 40px;
+        border: 1px solid #0000ff;
+      }
+
+      .bs-wrapper {
+        display: inline-block;
+        width: 380px;
+        height: 86px;
+        overflow: hidden;
+
+        .content {
+          display: inline-flex;
+          padding: 0;
+          margin: 0;
+          list-style: none;
+
+          li {
+            list-style: none;
+            width: 80px;
+            height: 100%;
+            padding: 0;
+            margin-right: 10px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
